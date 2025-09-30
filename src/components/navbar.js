43325d782b70ef649083a2ecf2c344fdc83d20cd
brandonyeu <brandonyeu8@gmail.com>
2025-09-30 13:26:41 -0400

    navbar done

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,14 +1,33 @@
+import { UserCircleIcon } from '@heroicons/react/24/solid';
+import Link from "next/link";
 
 export default function NavBar() {
     return (
-        <nav className="w-full h-16 bg-gray-800 text-white flex items-center justify-between px-8">
-            <a>
-                <img src="logo.png" alt="Logo" />
-            </a>
-            <div className="space-x-4">
-                <a href="/" className="hover:underline">Home</a>
-                <a href="/about" className="hover:underline">About</a>
-                <a href="/contact" className="hover:underline">Contact</a>
+        <nav className="w-full h-16 bg-white text-black flex items-center justify-between px-8">
+            {/* Left side: logo + nav links */}
+            <div className="flex items-center space-x-6">
+                <Link href="/">
+                    <img src="logo.png" alt="Logo" width="160" height="20" />
+                </Link>
+                <Link href="/matches" className="hover:underline hover:opacity-80">My Buddies</Link>
+                <Link href="/messages" className="hover:underline hover:opacity-80">Messages</Link>
+            </div>
+
+            {/* Right side: button + profile */}
+            <div className="flex items-center space-x-4">
+                <Link
+                    href="/form"
+                    className="py-2 px-4 rounded hover:underline hover:opacity-80"
+                    style={{
+                        backgroundImage: 'linear-gradient(to right, #59EE80, #56E9F1)'
+                    }}
+                >
+                    Find a Study Buddy
+                </Link>
+
+                <Link href="/profile" className="hover:opacity-80">
+                    <UserCircleIcon className="h-8 w-8" />
+                </Link>
             </div>
         </nav>
     );
