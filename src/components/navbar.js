07db6c5cc387c2f13d4fbb9ed5af3dfbfa00274e
brandonyeu <brandonyeu8@gmail.com>
2025-09-30 12:58:06 -0400

    navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,4 +1,15 @@
 
-export default function Home() {
-
+export default function NavBar() {
+    return (
+        <nav className="w-full h-16 bg-gray-800 text-white flex items-center justify-between px-8">
+            <a>
+                <img src="logo.png" alt="Logo" />
+            </a>
+            <div className="space-x-4">
+                <a href="/" className="hover:underline">Home</a>
+                <a href="/about" className="hover:underline">About</a>
+                <a href="/contact" className="hover:underline">Contact</a>
+            </div>
+        </nav>
+    );
 }
