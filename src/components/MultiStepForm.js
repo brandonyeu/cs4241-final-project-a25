8d8dcf98d1335de2ebfe5e463064ff59e8a9d4ba
Arianna Xie <ariannaxie7@gmail.com>
2025-10-01 11:12:28 -0400

    Added file structure for the multi-step form under components.

---
+++ src/components/MultiStepForm.js
@@ -0,0 +1,7 @@
+import { useState } from "react";
+import Step1 from "./formSteps/step1";
+import Step2 from "./formSteps/step2";
+import Step3 from "./formSteps/step3";
+import Step4 from "./formSteps/step4";
+import Step5 from "./formSteps/step5";
+import Step6 from "./formSteps/step6";
