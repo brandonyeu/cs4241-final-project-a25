2c5a1f2eacf6219c050cc2a80ae7071a50e35a54
cacsibal <cacsibal@gmail.com>
2025-10-05 14:57:06 -0400

    function headers for api calls

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,5 +1,17 @@
 import clientPromise from "@/lib/db";
 import { NextResponse } from "next/server";
 
+export async function POST(req, res) {
+
+}
+
 export async function GET(req, res) {
+}
+
+export async function DELETE(req, res) {
+
+}
+
+export async function PUT(req, res) {
+
 }
