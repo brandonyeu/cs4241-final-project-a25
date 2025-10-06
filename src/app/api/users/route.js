7aba4d2360e5b61d05903d2a26d1bb52b4c80bf2
cacsibal <cacsibal@gmail.com>
2025-10-05 23:39:43 -0400

    utilized states to record signup info, todo: fix post route

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,8 +1,41 @@
 import clientPromise from "@/lib/db";
-import { NextResponse } from "next/server";
+import {NextResponse} from "next/server";
 
-export async function POST(req, res) {
+export async function POST(req) {
+    try {
+        const formData = await req.json();
 
+        const client = await clientPromise;
+        const db = client.db("studi");
+        const collection = db.collection("form");
+
+        if (collection.find({email: req.body.email}).count() > 0) {
+            return new NextResponse(JSON.stringify({error: "Email already exists"}), {
+                    status: 409,
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }
+            )
+        }
+
+        const result = await collection.insertOne(formData);
+
+        return new NextResponse(JSON.stringify(
+                {
+                    success: true,
+                    message: "Form submitted successfully",
+                    id: result.insertedId,
+                }),
+            {
+                status: 201,
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+    } catch (err) {
+        console.error(err);
+    }
 }
 
 export async function GET(req, res) {
